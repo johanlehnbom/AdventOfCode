cf2e418fd90cf07c99505e0ab1b922c7e3b47a28
Johan Lehnbom <johan.lehnbom@lm.se>
2017-11-30 12:02:38 +0100

    DAY2 - Lagt till dag två.

---
+++ src/aoc/days/Day2.java
@@ -0,0 +1,11 @@
+package aoc.days;
+
+import javax.sound.midi.Soundbank;
+
+public class Day2 {
+
+    public Day2() {
+        System.out.println("Dag 2");
+    }
+
+}
--- src/aoc/days/Main.java
+++ src/aoc/days/Main.java
@@ -3,7 +3,9 @@ package aoc.days;
 public class Main {
 
     public static void main(String[] args) {
+
         new Day1();
+        new Day2();
     }
 
 }
